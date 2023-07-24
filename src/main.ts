ae51efebf7f76d6761b12252a576c82e2f41c173
462441355@qq.com <462441355@qq.com>
2023-07-24 15:56:19 +0800

    * 基础css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from "@/router"
 
-createApp(App).mount('#app')
+// css
+import "uno.css"
+import "normalize.css"
+import "element-plus/dist/index.css"
+import "element-plus/theme-chalk/dark/css-vars.css"
+import "@/assets/css/index.scss"
+
+const app = createApp(App)
+
+app.use(router)
+
+router.isReady().then(() => {
+  app.mount("#app")
+})
+
+
+// createApp(App).mount('#app')
