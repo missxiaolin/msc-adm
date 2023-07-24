bd118090a70befe9af36fd2d58fc93a95f9eeda7
462441355@qq.com <462441355@qq.com>
2023-07-24 16:17:40 +0800

    * 代码忘记保存

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './style.css'
 import App from './App.vue'
 import router from "@/router"
 import { loadPlugins } from "@/plugins"
+import { loadDirectives } from "@/directives"
 
 // css
 import "uno.css"
