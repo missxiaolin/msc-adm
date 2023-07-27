78bd12471bf8609d219c4915c87c4e71f6eac2f9
462441355@qq.com <462441355@qq.com>
2023-07-27 17:12:21 +0800

    * add

--- src/App.vue
+++ src/App.vue
@@ -6,42 +6,13 @@
 
 
 <script setup lang="ts">
-import { h } from "vue"
 import { useTheme } from "@/hooks/useTheme"
-import { resetConfigLayout } from "@/utils"
-import { ElNotification } from "element-plus"
 // 将 Element Plus 的语言设置为中文
 import zhCn from "element-plus/es/locale/lang/zh-cn"
 
 const { initTheme } = useTheme()
 /** 初始化主题 */
 initTheme()
-// ElNotification({
-//   title: "Hello",
-//   type: "success",
-//   message: h(
-//     "a",
-//     { style: "color: teal", target: "_blank", href: "" },
-//     ""
-//   ),
-//   duration: 0,
-//   position: "bottom-right"
-// })
-// ElNotification({
-//   title: "破坏性更新",
-//   type: "warning",
-//   message: h(
-//     "span",
-//     {
-//       style: "color: teal; cursor: pointer;",
-//       onClick: resetConfigLayout
-//     },
-//     ""
-//   ),
-//   duration: 0,
-//   position: "bottom-right",
-//   offset: 150
-// })
 </script>
 
 
