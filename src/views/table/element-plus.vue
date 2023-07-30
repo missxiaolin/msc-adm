21623d6c3bcfed6dcf0ff18dd0ce8bd70ec4a666
462441355@qq.com <462441355@qq.com>
2023-07-30 19:19:13 +0800

    * 饿了么表格

--- src/views/table/element-plus.vue
+++ src/views/table/element-plus.vue
@@ -41,6 +41,70 @@
           </el-tooltip>
         </div>
       </div>
+      <div class="table-wrapper">
+        <el-table :data="tableData">
+          <el-table-column type="selection" width="50" align="center" />
+          <el-table-column prop="username" label="用户名" align="center" />
+          <el-table-column prop="roles" label="角色" align="center">
+            <template #default="scope">
+              <el-tag v-if="scope.row.roles === 'admin'" effect="plain"
+                >admin</el-tag
+              >
+              <el-tag v-else type="warning" effect="plain">{{
+                scope.row.roles
+              }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="phone" label="手机号" align="center" />
+          <el-table-column prop="email" label="邮箱" align="center" />
+          <el-table-column prop="status" label="状态" align="center">
+            <template #default="scope">
+              <el-tag v-if="scope.row.status" type="success" effect="plain"
+                >启用</el-tag
+              >
+              <el-tag v-else type="danger" effect="plain">禁用</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="createTime" label="创建时间" align="center" />
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="150"
+            align="center"
+          >
+            <template #default="scope">
+              <el-button
+                type="primary"
+                text
+                bg
+                size="small"
+                @click="handleUpdate(scope.row)"
+                >修改</el-button
+              >
+              <el-button
+                type="danger"
+                text
+                bg
+                size="small"
+                @click="handleDelete(scope.row)"
+                >删除</el-button
+              >
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div class="pager-wrapper">
+        <el-pagination
+          background
+          :layout="paginationData.layout"
+          :page-sizes="paginationData.pageSizes"
+          :total="paginationData.total"
+          :page-size="paginationData.pageSize"
+          :currentPage="paginationData.currentPage"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </div>
     </el-card>
     <!-- 新增/修改 -->
     <el-dialog
@@ -59,10 +123,7 @@
         <el-form-item prop="username" label="用户名">
           <el-input v-model="formData.username" placeholder="请输入" />
         </el-form-item>
-        <el-form-item
-          prop="password"
-          label="密码"
-        >
+        <el-form-item prop="password" label="密码">
           <el-input v-model="formData.password" placeholder="请输入" />
         </el-form-item>
       </el-form>
@@ -90,6 +151,8 @@ import {
   Download,
   RefreshRight,
 } from "@element-plus/icons-vue";
+import { type GetTableData } from "@/api/table/types/table";
+import { usePagination } from "@/hooks/usePagination"
 export default {
   name: "ElementPlus",
   setup() {
@@ -98,6 +161,7 @@ export default {
       username: "",
       phone: "",
     });
+    const tableData = ref<GetTableData[]>([]);
     const searchFormRef = ref<FormInstance | null>(null);
     //#region 增
     const dialogVisible = ref<boolean>(false);
@@ -109,6 +173,8 @@ export default {
       username: [{ required: true, trigger: "blur", message: "请输入用户名" }],
       password: [{ required: true, trigger: "blur", message: "请输入密码" }],
     });
+    const { paginationData, handleCurrentChange, handleSizeChange } = usePagination()
+
     const handleSearch = () => {
       console.log("handleSearch");
     };
@@ -127,9 +193,29 @@ export default {
     };
 
     const handleCreate = () => {
-        console.log('handleCreate')
-        dialogVisible.value = false
-    }
+      console.log("handleCreate");
+      dialogVisible.value = false;
+    };
+
+    tableData.value = [
+      {
+        createTime: "2021-01-01",
+        email: "462441355@qq.com",
+        id: "1",
+        phone: "171***1105",
+        roles: "admin",
+        status: true,
+        username: "xiaolin",
+      },
+    ];
+
+    const handleUpdate = (row: GetTableData) => {
+      formData.username = row.username;
+      dialogVisible.value = true;
+    };
+    const handleDelete = (row: GetTableData) => {
+      console.log(row);
+    };
 
     return {
       loading,
@@ -147,7 +233,13 @@ export default {
       resetForm,
       formRules,
       handleCreate,
-      formData
+      formData,
+      tableData,
+      handleUpdate,
+      handleDelete,
+      paginationData,
+      handleCurrentChange,
+      handleSizeChange
     };
   },
 };
