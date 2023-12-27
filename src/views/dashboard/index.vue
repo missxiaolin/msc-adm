58d5eb550b58fb16c9ded69b492e5b5d25c3b436
lingongbing <462441355@qq.com>
2023-12-27 15:28:13 +0800

    * 主体影响

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -3,71 +3,38 @@
     <dl class="home-title flex">
       <dt class="flex-1">核心数据</dt>
       <dd>
-        <el-date-picker
-          v-model="data.analyseTime"
-          type="date"
-          value-format="YYYY-MM-DD"
-          placeholder="请选择查询日期"
-          :clearable="false"
-          :disabled-date="disabledDate"
-          @change="initQuery"
-        />
+        <el-date-picker v-model="data.analyseTime" type="date" value-format="YYYY-MM-DD" placeholder="请选择查询日期"
+          :clearable="false" :disabled-date="disabledDate" @change="initQuery" />
       </dd>
     </dl>
     <el-card class="box-card">
       <div class="top-title">
         流量数据
         <span style="color: var(--e-color-success-light-3)">(H-1数据)</span>
-        <el-tooltip
-          effect="dark"
-          content="定时数据，每小时59分钟59秒统计"
-          placement="right"
-        >
-          <el-icon size="20"><WarningFilled /></el-icon>
+        <el-tooltip effect="dark" content="定时数据，每小时59分钟59秒统计" placement="right">
+          <el-icon size="20">
+            <WarningFilled />
+          </el-icon>
         </el-tooltip>
       </div>
       <el-row :gutter="24">
         <el-col :span="4">
-          <dataCardItem
-            :optionData="data"
-            :title="'浏览量(PV)'"
-            :objKey="'pvCount'"
-          />
+          <dataCardItem :optionData="data" :title="'浏览量(PV)'" :objKey="'pvCount'" />
         </el-col>
         <el-col :span="4">
-          <dataCardItem
-            :optionData="data"
-            :title="'访客数(UV)'"
-            :objKey="'uvCount'"
-          />
+          <dataCardItem :optionData="data" :title="'访客数(UV)'" :objKey="'uvCount'" />
         </el-col>
         <el-col :span="4">
-          <dataCardItem
-            :optionData="data"
-            :title="'新访客'"
-            :objKey="'newUvCount'"
-          />
+          <dataCardItem :optionData="data" :title="'新访客'" :objKey="'newUvCount'" />
         </el-col>
         <el-col :span="4">
-          <dataCardItem
-            :optionData="data"
-            :title="'IP数'"
-            :objKey="'ipCounct'"
-          />
+          <dataCardItem :optionData="data" :title="'IP数'" :objKey="'ipCounct'" />
         </el-col>
         <el-col :span="4">
-          <dataCardItem
-            :optionData="data"
-            :title="'频次(人均)'"
-            :objKey="'visitFrequency'"
-          />
+          <dataCardItem :optionData="data" :title="'频次(人均)'" :objKey="'visitFrequency'" />
         </el-col>
         <el-col :span="4">
-          <dataCardItem
-            :optionData="data"
-            :title="'跳出率'"
-            :objKey="'jumpCount'"
-          />
+          <dataCardItem :optionData="data" :title="'跳出率'" :objKey="'jumpCount'" />
         </el-col>
       </el-row>
     </el-card>
@@ -78,11 +45,7 @@
     <dl class="home-title flex">
       <dt>综合数据</dt>
       <dd class="flex-1">
-        <el-radio-group
-          v-model="chart.showNum"
-          @change="handleTopNumChange"
-          size="small"
-        >
+        <el-radio-group v-model="chart.showNum" @change="handleTopNumChange" size="small">
           <el-radio-button :label="10">10</el-radio-button>
           <el-radio-button :label="20">20</el-radio-button>
           <el-radio-button :label="30">30</el-radio-button>
@@ -92,70 +55,53 @@
     <section class="synthetic-data flex">
       <div class="synthetic-data flex">
         <div class="e-col">
-          <colItem
-            :chart="chart"
-            :optionData="data"
-            :chartKey="'simpleUrl'"
-            :title="'页面访问量TOP'"
-            :tip="'统计网站的访问数量'"
-          />
+          <el-card>
+            <colItem :chart="chart" :optionData="data" :chartKey="'simpleUrl'" :title="'页面访问量TOP'" :tip="'统计网站的访问数量'" />
+          </el-card>
         </div>
         <div class="e-col">
-          <colItem
-            :chart="chart"
-            :optionData="data"
-            :chartKey="'cregion'"
-            :title="'城市名称访问用户量TOP'"
-            :tip="'在不同城市访问网站的用户量分布情况。ps: 城市是根据ip地址查询出来的，并不是每个ip都能查询出来，查询出来的结果也未必准确，只作为趋势参考'"
-          />
+          <el-card>
+            <colItem :chart="chart" :optionData="data" :chartKey="'cregion'" :title="'城市名称访问用户量TOP'"
+              :tip="'在不同城市访问网站的用户量分布情况。ps: 城市是根据ip地址查询出来的，并不是每个ip都能查询出来，查询出来的结果也未必准确，只作为趋势参考'" />
+          </el-card>
+
         </div>
         <div class="e-col">
-          <colItem
-            :chart="chart"
-            :optionData="data"
-            :chartKey="'device'"
-            :title="'浏览设备访问用户量TOP'"
-            :tip="'数值代表使用的浏览设备的数量'"
-          />
+          <el-card>
+            <colItem :chart="chart" :optionData="data" :chartKey="'device'" :title="'浏览设备访问用户量TOP'"
+              :tip="'数值代表使用的浏览设备的数量'" />
+          </el-card>
+
         </div>
         <div class="e-col">
-          <colItem
-            :chart="chart"
-            :optionData="data"
-            :chartKey="'os'"
-            :title="'系统版本访问用户量TOP'"
-            :tip="'不同设备星号的用户量分布情况'"
-          />
+          <el-card>
+            <colItem :chart="chart" :optionData="data" :chartKey="'os'" :title="'系统版本访问用户量TOP'" :tip="'不同设备星号的用户量分布情况'" />
+          </el-card>
+
         </div>
         <div class="e-col">
-          <colItem
-            :chart="chart"
-            :optionData="data"
-            :chartKey="'browser'"
-            :title="'浏览器版本访问用户量TOP'"
-            :tip="'数值代表使用的浏览器版本的数量'"
-          />
+          <el-card>
+            <colItem :chart="chart" :optionData="data" :chartKey="'browser'" :title="'浏览器版本访问用户量TOP'"
+              :tip="'数值代表使用的浏览器版本的数量'" />
+          </el-card>
+
         </div>
         <div class="e-col">
-          <colItem
-            :chart="chart"
-            :optionData="data"
-            :chartKey="'screen'"
-            :title="'设备分辨率访问用户量TOP'"
-            :tip="'数值代表设备的物理分辨率'"
-          />
+          <el-card>
+            <colItem :chart="chart" :optionData="data" :chartKey="'screen'" :title="'设备分辨率访问用户量TOP'"
+              :tip="'数值代表设备的物理分辨率'" />
+          </el-card>
+
         </div>
       </div>
     </section>
     <el-card class="box-card mt20">
       <div class="top-title">
         地理分布
-        <el-tooltip
-          effect="dark"
-          content="城市是根据ip地址查询出来的，并不是每个ip都能查询出结果，查询出来的结果也未必准确，只作为趋势参考。"
-          placement="right"
-        >
-          <el-icon size="20"><WarningFilled /></el-icon>
+        <el-tooltip effect="dark" content="城市是根据ip地址查询出来的，并不是每个ip都能查询出结果，查询出来的结果也未必准确，只作为趋势参考。" placement="right">
+          <el-icon size="20">
+            <WarningFilled />
+          </el-icon>
         </el-tooltip>
       </div>
       <MapEcharts :ipCregion="chart.ipcregion" />
@@ -314,46 +260,50 @@ export default {
 .home-title {
   padding: 10px 0;
   color: #000;
+
   dt {
     line-height: 32px;
     font-weight: 700;
     font-size: 18px;
     padding-right: 20px;
   }
+
   dt,
   dd {
     display: flex;
     align-items: center;
   }
 }
+
 .box-card {
   width: 100%;
   display: flex;
+
   :deep(.el-card__body) {
     width: 100%;
   }
+
   .top-title {
     margin-bottom: 20px;
+
     :deep(.el-icon) {
       vertical-align: middle;
       margin-left: 5px;
     }
   }
 }
+
 .synthetic-data {
   width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   flex-wrap: wrap;
+
   .e-col {
     flex-direction: column;
-    background-color: #fff;
-    border-radius: 6px;
-    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-    border-radius: 10px;
-    background-color: #fff;
     width: 32.5%;
+
     &:nth-child(4),
     &:nth-child(5),
     &:nth-child(6) {
