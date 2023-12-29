dcb39dac09ed775fbc29b8f4d9164fc94ffb36f5
lingongbing <462441355@qq.com>
2023-12-29 12:22:47 +0800

    * 增加判断

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -87,6 +87,14 @@ function createService() {
   return service;
 }
 
+let baseURL = "http:msc-serve.missxiaolin.com"
+
+console.log()
+
+if (window.location.href.indexOf('http://localhost') > -1) {
+  baseURL = "http://127.0.0.1:9001"
+}
+
 /** 创建请求方法 */
 function createRequest(service: AxiosInstance) {
   return function <T>(config: AxiosRequestConfig): Promise<T> {
@@ -99,7 +107,7 @@ function createRequest(service: AxiosInstance) {
         "Content-Type": "application/json",
       },
       timeout: 5000,
-      baseURL: "http://127.0.0.1:9001",
+      baseURL: baseURL,
       data: {},
     };
     // 将默认配置 defaultConfig 和传入的自定义配置 config 进行合并成为 mergeConfig
