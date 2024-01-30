01728f253f41a996a5171bcdf4ae83d46d1fdf6e
lingongbing <462441355@qq.com>
2024-01-30 13:48:09 +0800

    * 打包

---
+++ dist/static/index-3fabd8d2.js
@@ -0,0 +1,2 @@
+import{r as a}from"./service-14fff055.js";function t(t){return a({url:"/adm/sourcemap/list",method:"post",data:t})}function r(t){return a({url:"/adm/sourcemap/version/list",method:"post",data:t})}function s(t){return a({url:"/adm/sourcemap/analysis",method:"post",data:t})}export{r as a,t as b,s};
+//# sourceMappingURL=index-3fabd8d2.js.map
